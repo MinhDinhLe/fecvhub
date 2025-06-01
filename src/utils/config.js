c724cb8092f0b3b439d4c8f92cfc8e05c238ce7b
MinhDinhLe <147137889+MinhDinhLe@users.noreply.github.com>
2025-06-01 18:42:56 +0700

    adda

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://cvhubmth-30a934eec9be.herokuapp.com/api';
-export const API_URL = 'http://localhost:8080/api';
+export const API_URL = 'https://cvhubmth-30a934eec9be.herokuapp.com/api';
+// export const API_URL = 'http://localhost:8080/api';
